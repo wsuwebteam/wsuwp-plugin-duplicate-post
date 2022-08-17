3ea34608f129a429ef49f194b175c6c5cfb8db32
Dan White <dan.white1@wsu.edu>
2022-08-17 10:43:59 -0700

    Fix | Activation

--- wsuwp-plugin-duplicate-post.php
+++ wsuwp-plugin-duplicate-post.php
@@ -3,7 +3,7 @@
  * Plugin Name: WSUWP Plugin | Duplicate Post
  * Plugin URI: https://github.com/wsuwebteam/wsuwp-plugin-duplicate-post
  * Description: Adds a duplicate post button to the admin bar.
- * Version:    1.0.2
+ * Version:    1.0.2.1
  * Requires PHP: 7.3
  * Author: Washington State University, Dan White
  * Author URI: https://web.wsu.edu/
@@ -17,6 +17,15 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 // Initiate plugin
-if ( is_admin() && defined( 'ISWDS' ) ) {
-	require_once __DIR__ . '/includes/plugin.php';
+add_action( 'after_setup_theme', 'wsuwp_plugin_init' );
+
+function wsuwp_plugin_init() {
+
+	if ( is_admin() && defined( 'ISWDS' ) ) {
+
+		// Initiate plugin
+		require_once __DIR__ . '/includes/plugin.php';
+
+	}
+
 }
